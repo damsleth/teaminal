ed523abf30ee33c51b3f6ccdf30eb8f48fda4ab4
Carl Joakim Damsleth <kim@damsleth.no>
2026-08-25 15:42:23 +0200

    chore: mirror homebrew formula for v0.22.0

--- docs/homebrew/teaminal.rb
+++ docs/homebrew/teaminal.rb
@@ -1,7 +1,7 @@
 class Teaminal < Formula
   desc "Lightweight terminal Microsoft Teams client"
   homepage "https://github.com/damsleth/teaminal"
-  version "0.21.1"
+  version "0.22.0"
   license "MIT"
 
   depends_on "damsleth/tap/owa-piggy" => :recommended
@@ -9,20 +9,20 @@ class Teaminal < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/damsleth/teaminal/releases/download/v#{version}/teaminal-#{version}-darwin-arm64.tar.gz"
-      sha256 "65d3c3634229d67ce26d7c95e1243bdc9c1ed7dea73e8df818f0ac4790c0cb7a"
+      sha256 "9a712d2ca2e1674d480e1775ad349c34b5e350d8ea0a95a9a18e378c5ab9b2da"
     else
       url "https://github.com/damsleth/teaminal/releases/download/v#{version}/teaminal-#{version}-darwin-x64.tar.gz"
-      sha256 "1ee92af504192ecadec75c5249b3677a1a361dc79135e4e740ff4eaedf9d8410"
+      sha256 "05f86c9e1f0351efbc3f60f17b4d04aa9d2b8a8d56c5afb1e7185c1005dae625"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/damsleth/teaminal/releases/download/v#{version}/teaminal-#{version}-linux-arm64.tar.gz"
-      sha256 "6d1f43a29c4a530bf2fad87ad9c8f8f68763896d1264818d422a830053c5d857"
+      sha256 "3bae85a6d10c54c7a3e3463ab55e8745ec5e4308b76f5544c745cf4b42b56cc2"
     else
       url "https://github.com/damsleth/teaminal/releases/download/v#{version}/teaminal-#{version}-linux-x64.tar.gz"
-      sha256 "3ae0660301f80ed23bc6d7b24a5af031ef16edd6b2e92151195fedf44960057e"
+      sha256 "9c3d20af85e1244f941965a535fbfcb364ad695d949638e6f09a2d65e0144341"
     end
   end
 
